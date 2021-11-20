c560f54de8c27c1b440af96eb3a7f0c0cde4892b
pankaj.k.jha <pankaj.k.jha@accenture.com>
2021-11-20 18:58:38 +0800

    Added Framework

---
+++ InsurancePremium.xcframework/ios-arm64/InsurancePremium.framework/Headers/InsurancePremium.h
@@ -0,0 +1,18 @@
+//
+//  InsurancePremium.h
+//  InsurancePremium
+//
+//  Created by pankaj.k.jha on 20/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InsurancePremium.
+FOUNDATION_EXPORT double InsurancePremiumVersionNumber;
+
+//! Project version string for InsurancePremium.
+FOUNDATION_EXPORT const unsigned char InsurancePremiumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InsurancePremium/PublicHeader.h>
+
+
